8b13fd9216a1b197105ccdfa97ed9e8cb11aa3e2
omer hayoona <omer.hayoon@gmail.com>
2024-02-13 11:28:34 +0200

    new 13.2.24

--- src/Game.js
+++ src/Game.js
@@ -16,18 +16,18 @@ class Game extends React.Component {
     timerInterval;
 
     updateTurn = () => {
-        const {currentPlayer, timer} = this.state;
+        const { currentPlayer, timer } = this.state;
         const newCurrentPlayer = currentPlayer === 1 ? 2 : 1;
-        this.setState({currentPlayer: newCurrentPlayer, timer: 10});
+        this.setState({ currentPlayer: newCurrentPlayer, timer: 10 });
         clearInterval(this.timerInterval);
         this.startTimer();
     };
 
     startTimer = () => {
         this.timerInterval = setInterval(() => {
-            const {timer} = this.state;
+            const { timer } = this.state;
             if (timer > 0) {
-                this.setState({timer: timer - 1});
+                this.setState({ timer: timer - 1 });
             } else {
                 clearInterval(this.timerInterval);
                 this.updateTurn();
@@ -36,42 +36,44 @@ class Game extends React.Component {
     };
 
     changeCurrentPlayer = (col) => {
-        const {currentPlayer, player1Color, player2Color, board, rows} = this.state;
+        const { currentPlayer, player1Color, player2Color, board, rows } = this.state;
         const newCurrentPlayer = currentPlayer === 1 ? 2 : 1;
-
         const updatedBoard = [...board];
         const playerColor = currentPlayer === 1 ? player1Color : player2Color;
         let boardFull = true;
-
         for (let i = rows - 1; i >= 0; i--) {
             if (updatedBoard[i][col].color === 'white') {
-                updatedBoard[i][col] = {color: playerColor, selected: true};
-                this.checkForVictory(i, col)
+                updatedBoard[i][col] = { color: playerColor, selected: true };
+                this.checkForVictory(i, col);
                 boardFull = false;
                 break;
             }
         }
         if (!boardFull) {
-            this.setState({board: updatedBoard, currentPlayer: newCurrentPlayer, timer: 10});
+            this.setState({ board: updatedBoard, currentPlayer: newCurrentPlayer, timer: 10 });
             clearInterval(this.timerInterval);
             this.startTimer();
         } else {
             window.alert("Column is full, choose another one.");
         }
-
     };
 
     checkForVictory = (row, col) => {
-        const {board, currentPlayer, player1Color, player2Color} = this.state;
+        const { board, currentPlayer, player1Color, player2Color, timer } = this.state;
         const playerColor = currentPlayer === 1 ? player1Color : player2Color;
-        if (this.checkRow(row, playerColor) || this.checkColumn(col, playerColor)) {
+        if (
+            this.checkRow(row, playerColor) ||
+            this.checkColumn(col, playerColor) ||
+            this.checkDiagonal(row, col, playerColor)
+        ) {
             alert('Player ' + JSON.stringify(currentPlayer) + ' wins!');
             this.resetGame();
         }
     };
 
+
     checkRow = (row, playerColor) => {
-        const {board} = this.state;
+        const { board } = this.state;
         let count = 0;
         for (let i = 0; i < board[row].length; i++) {
             if (board[row][i].color === playerColor) {
@@ -87,7 +89,7 @@ class Game extends React.Component {
     };
 
     checkColumn = (col, playerColor) => {
-        const {board} = this.state;
+        const { board } = this.state;
         let count = 0;
         for (let i = 0; i < board.length; i++) {
             if (board[i][col].color === playerColor) {
@@ -102,34 +104,62 @@ class Game extends React.Component {
         return false;
     };
 
+    checkDiagonal = (row, col, playerColor) => {
+        return (
+            this.checkDiagonalDirection(row, col, playerColor, -1, 1) || // Diagonal upwards
+            this.checkDiagonalDirection(row, col, playerColor, 1, 1) || // Diagonal downwards
+            this.checkDiagonalDirection(row, col, playerColor, -1, -1) || // Diagonal upwards (other direction)
+            this.checkDiagonalDirection(row, col, playerColor, 1, -1) // Diagonal downwards (other direction)
+        );
+    };
+
+    checkDiagonalDirection = (row, col, playerColor, rowIncrement, colIncrement) => {
+        const { board } = this.state;
+        const rows = board.length;
+        const cols = board[0].length;
+        let count = 0;
+        let currentRow = row;
+        let currentCol = col;
+        while (
+            currentRow >= 0 &&
+            currentRow < rows &&
+            currentCol >= 0 &&
+            currentCol < cols &&
+            board[currentRow][currentCol].color === playerColor
+            ) {
+            count++;
+            currentRow += rowIncrement;
+            currentCol += colIncrement;
+        }
+        return count >= 4;
+    };
+
     resetGame = () => {
-        clearInterval(this.timerInterval);
         this.setState({
             rows: "",
             cols: "",
-            currentPlayer: 1,
+            currentPlayer: "",
             player1Color: "",
             player2Color: "",
             board: [],
-            timer: 0,
+            timer: "",
         });
-
     };
+
     createBoard = () => {
-        const {rows, cols} = this.state;
-        const board = Array(rows).fill().map(() => Array(cols).fill({color: 'white', selected: false}));
-        this.setState({board, timer: 10}, () => {
-            this.startTimer(); // נתחיל את הטיימר רק לאחר יצירת לוח המשחק
+        const { rows, cols } = this.state;
+        const board = Array(rows).fill().map(() => Array(cols).fill({ color: 'white', selected: false }));
+        this.setState({ board, timer: 10 }, () => {
+            this.startTimer();
         });
     };
 
     changeSize = (event) => {
         const newSize = parseInt(event.target.value, 10);
         if (newSize >= 5 && newSize <= 10) {
-            this.setState({rows: newSize, cols: newSize});
+            this.setState({ rows: newSize, cols: newSize });
         }
     };
-
 
     updatePlayerColor = (event, playerNumber) => {
         const selectedColor = event.target.value;
@@ -137,12 +167,11 @@ class Game extends React.Component {
 
         if (selectedColor !== otherPlayerColor) {
             const playerColorKey = playerNumber === 1 ? 'player1Color' : 'player2Color';
-            this.setState({[playerColorKey]: selectedColor});
+            this.setState({ [playerColorKey]: selectedColor });
         } else {
             alert('Please choose a different color from the other player.');
         }
     };
-
 
     componentWillUnmount() {
         clearInterval(this.timerInterval);
@@ -150,12 +179,10 @@ class Game extends React.Component {
 
     render = () => {
         const backgroundURL = 'url("https://stuntsoftware.com/img/4inarow/title.png")';
-        const {board, rows, colorOptions, player1Color, player2Color, timer} = this.state;
+        const { board, rows, colorOptions, player1Color, player2Color, timer } = this.state;
         return (
-            <h2 style={{backgroundImage: backgroundURL, backgroundRepeat: 'no-repeat', backgroundPosition: '30% -5px'}}>
-                <h1 style={{color: "darkgreen"}}>4 in a row game !</h1>
-                <div>Current player : {this.state.currentPlayer} </div>
-                <div>Time left : {timer} seconds</div>
+            <h1 style={{backgroundImage: backgroundURL, backgroundRepeat: 'no-repeat', backgroundPosition: '30% -5px'}}>
+                <h2 style={{color: "darkgreen"}}>4 in a row game !</h2>
                 <div>
                     Player 1 Color :
                     <select value={player1Color} disabled={board.length > 0}
@@ -181,10 +208,11 @@ class Game extends React.Component {
                 <div>
                     <Board createBoard={board} changeCurrentPlayer={this.changeCurrentPlayer}/>
                 </div>
+                <div>Current player : {this.state.currentPlayer} </div>
+                <div>Time left : {timer} seconds</div>
                 <div>
-
                 </div>
-            </h2>
+            </h1>
         );
     };
 }
